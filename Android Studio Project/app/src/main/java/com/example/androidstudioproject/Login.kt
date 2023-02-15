74eb00a314f317896d5775e7e687f9a99283802e
coutJP <jeanpaul_mansour@outlook.com>
2023-02-15 22:05:57 +0200

    authentication linked to data base

--- Android Studio Project/app/src/main/java/com/example/androidstudioproject/Login.kt
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/Login.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
     private lateinit var edtEmail:EditText
@@ -12,21 +15,50 @@ class Login : AppCompatActivity() {
     private lateinit var btnLogin:Button
     private lateinit var btnSignUp:Button
 
+    //firebase auth
+    private lateinit var mAuth:FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        edtEmail=findViewById(R.id.edt_email)
-        edtPassword=findViewById(R.id.edt_password)
-        btnLogin=findViewById(R.id.btnLogin)
-        btnSignUp=findViewById(R.id.btnSignUp)
+        supportActionBar?.hide()//to hide actionbar
+        mAuth = FirebaseAuth.getInstance()//firebase authentication
+
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
         //to when signup btn press go to signup w zabatet l inten bl manifest  hatyta b login ta y2ale3 login msh main activity
-        btnSignUp.setOnClickListener{
-            val intent=Intent(this,SignUp::class.java)
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
 
+        //shubdo ysir onclicking login
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email, password);
+        }
+
+    }
+
+        private fun login(email:String,password:String){
+            //logic for the loging in user
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        //code for loging in user
+                        val  intent=Intent(this@Login,MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this@Login,"User doess not exist",Toast.LENGTH_SHORT).show()
+                    }
+                }
 
 
     }
--- Android Studio Project/app/src/main/java/com/example/androidstudioproject/SignUp.kt
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/SignUp.kt
@@ -1,11 +1,53 @@
 package com.example.androidstudioproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+    //firebase auth
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        supportActionBar?.hide()//to hide actionbar
+
+
+        mAuth = FirebaseAuth.getInstance()//firebase authentication
+
+        edtEmail=findViewById(R.id.edt_name)
+        edtEmail=findViewById(R.id.edt_email)
+        edtPassword=findViewById(R.id.edt_password)
+        btnSignUp=findViewById(R.id.btnSignUp)
+        //to when signup btn press go to signup w zabatet l inten bl manifest  hatyta b login ta y2ale3 login msh main activity
+        btnSignUp.setOnClickListener{
+            val email= edtEmail.text.toString()
+            val password=edtPassword.text.toString()
+
+            signUp(email,password)
+        }
     }
-}
+        private fun signUp(email:String,password:String){
+            //logic of creating user
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        //code for jumping to home
+                        val  intent=Intent(this@SignUp,MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this@SignUp,"Some error occured",Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
