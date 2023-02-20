f3122d9b9237309a64d34a1e0c845c3b922368fd
coutJP <jeanpaul_mansour@outlook.com>
2023-02-20 17:45:48 +0200

    we can create channel and found it in firebase

---
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/TextChannelCreated.kt
@@ -0,0 +1,11 @@
+package com.example.androidstudioproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TextChannelCreated : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_channel_created)
+    }
+}
