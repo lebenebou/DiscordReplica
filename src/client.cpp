4e768e6c689c177cb9fa251ea06825be8e79317d
Youssef Yammine <lebenebou@gmail.com>
2023-02-24 15:42:48 +0200

    added client that can talk to the server

---
+++ src/client.cpp
@@ -0,0 +1,63 @@
+
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int clientSocket= socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket== -1){
+        cout << "Couldn't create socket" << endl;
+        return 1;
+    }
+
+    int port = 54000;
+    string ipAddress = "127.0.0.1";
+
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+    int result = connect(clientSocket, (sockaddr*)&hint, sizeof(hint));
+    if (result == -1){
+        cout << "Couldn't connect to server. Server might not be online" << endl;
+        return 1;
+    }
+
+    char buf[4096];
+    string userInput;
+
+    while(true){
+
+        cout << "> ";
+        getline(cin, userInput); // get input from user
+
+        int result = send(clientSocket, userInput.c_str(), userInput.size() + 1, 0);
+        if (result == -1){
+            cout << "Could not send to server.";
+            continue;
+        }
+
+        memset(buf, 0, 4096); // clear buffer array
+        int bytesReceived = recv(clientSocket, buf, 4096, 0); // receive bytes from the server
+
+        if (bytesReceived == -1){
+            cout << "Couldn't get response from server" << endl;
+        }
+        else{
+            cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+        }
+    }
+
+    close(clientSocket);
+
+    return 0;
+}
--- src/server.cpp
+++ src/server.cpp
@@ -12,33 +12,29 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
 
     int listening = socket(AF_INET, SOCK_STREAM, 0);
     if(listening < 0){
-
         cout << "Error when creating socket" << endl;
         return -1;
     }
     cout << "Created socket" << endl;
 
-    const int port = 54002;
+    const int port = 54000;
 
     sockaddr_in hint;
     hint.sin_family = AF_INET;
     hint.sin_port = htons(port);
-    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // 0.0.0.0 is to get any random address
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // 0.0.0.0 is to get any address
 
     if(bind(listening, (struct sockaddr*)&hint, sizeof(hint))){
-
         cout << "Couldn't bind to IP/port" << endl;
         return -2;
     }
     cout << "Binded IP/Port" << endl;
 
     if(listen(listening, SOMAXCONN) == -1){
-
         cout << "Couldn't listen" << endl;
         return -3;
     }
@@ -60,8 +56,8 @@ int main(int argc, char const *argv[])
 
     close(listening);
 
-    memset(host, 0, NI_MAXHOST); // clean array
-    memset(svc, 0, NI_MAXSERV); // clean array
+    memset(host, 0, NI_MAXHOST); // clear array
+    memset(svc, 0, NI_MAXSERV); // clear array
 
     int result = getnameinfo((struct sockaddr*)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
     if(result > 0){
@@ -72,27 +68,31 @@ int main(int argc, char const *argv[])
         cout << host << " connected on " << ntohs(client.sin_port) << endl;
     }
     cout << endl;
+    
+    // Connection established
+    const size_t buffer_size = 4096;
+    char buffer[buffer_size];
+    string received_message;
 
-    char buffer[4096];
     while(true){
 
-        memset(buffer, 0, 4096);
-        int bytesReceived = recv(clientSocket, buffer, 4096, 0);
+        memset(buffer, 0, buffer_size); // clear buffer array
+        int bytesReceived = recv(clientSocket, buffer, buffer_size, 0);
 
         if(bytesReceived == -1){
-            cout << "Connection issue" << endl;
+            cout << "Connection issue: couldn't receive bytes" << endl;
             break;
         }
         else if(bytesReceived == 0){
             cout << "Client diconnected" << endl;
             break;
         }
-
-        cout << "Received: " << string(buffer, 0, bytesReceived) << endl;
-        // send(clientSocket, buffer, bytesReceived+1, 0); // send back to client
+        
+        received_message = string(buffer, 0, bytesReceived);
+        cout << "Received: " << received_message << endl;
+        send(clientSocket, buffer, bytesReceived+1, 0); // send back to client
     }
 
     close(clientSocket);
-
     return 0;
 }
