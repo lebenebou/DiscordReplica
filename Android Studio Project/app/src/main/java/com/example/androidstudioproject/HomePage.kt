76199c7ad6f0ee92d0f392685f849b58f0530b8c
Youssef Yammine <lebenbou@gmail.com>
2023-03-28 13:37:53 +0300

    added join and create buttons that do nothing

--- Android Studio Project/app/src/main/java/com/example/androidstudioproject/HomePage.kt
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/HomePage.kt
@@ -2,11 +2,20 @@ package com.example.androidstudioproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomePage : AppCompatActivity() {
+
+    private lateinit var joinButton: Button
+    private lateinit var createButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        joinButton = findViewById(R.id.joinButton)
+        createButton = findViewById(R.id.createButton)
+
+
     }
 }
