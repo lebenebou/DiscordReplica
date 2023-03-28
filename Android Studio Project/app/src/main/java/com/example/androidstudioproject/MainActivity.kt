c882a0973dd1a9b440ece48b7a69605e9ef5dd02
Youssef Yammine <lebenbou@gmail.com>
2023-03-28 08:31:12 +0300

    removed firebase auth from mainActivity

--- Android Studio Project/app/src/main/java/com/example/androidstudioproject/MainActivity.kt
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/MainActivity.kt
@@ -5,32 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mAuth: FirebaseAuth //logout
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mAuth=FirebaseAuth.getInstance()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
         menuInflater.inflate(R.menu.menu,menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
         if(item.itemId==R.id.logout){
-            //write the logic for logout
-            mAuth.signOut()
-            val intent=Intent(this@MainActivity,Login::class.java)
+
+            // logic for logout
+            val intent = Intent(this@MainActivity, Login::class.java)
             finish()
             startActivity(intent)
-            return true
+            return false
         }
-        return true
+        return false
     }
 }
