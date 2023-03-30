7a0901e9a0906bf4e19df6f01268524bf14d6e94
Youssef Yammine <lebenbou@gmail.com>
2023-03-30 17:54:38 +0300

    implemented chat room correct timestamps

--- Android Studio Project/app/src/main/java/com/example/androidstudioproject/ChatRoom.kt
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/ChatRoom.kt
@@ -1,13 +1,75 @@
 package com.example.androidstudioproject
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ChatRoom : AppCompatActivity() {
+
+    private lateinit var messageList: LinearLayout
+    private lateinit var sendButton: Button
+    private lateinit var messageInput: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
+
+        messageList = findViewById(R.id.message_list)
+        messageInput = findViewById(R.id.messageInput)
+        sendButton = findViewById(R.id.sendButton)
+        sendButton.setOnClickListener{
+
+            val newMessage = JSONObject().apply {
+                put("username", GlobalVars.currentUser)
+                put("content", messageInput.text.toString())
+                put("timestamp", currentTimestamp())
+            }
+
+            addMessage(newMessage)
+            messageInput.text.clear()
+        }
+    }
+    private fun addMessage(message: JSONObject){
+
+        val messageLayout = LinearLayout(this)
+        messageLayout.orientation = LinearLayout.VERTICAL
+        messageLayout.setPadding(20, 20, 20, 20)
+
+        // Username
+        val usernameTextView = TextView(this)
+        usernameTextView.text = message.getString("username")
+        messageLayout.addView(usernameTextView)
+
+        // Content
+        val contentTextView = TextView(this)
+        contentTextView.text = message.getString("content")
+        messageLayout.addView(contentTextView)
+
+        // Timestamp
+        val timestampTextView = TextView(this)
+        timestampTextView.text = timestampToString(message.getLong("timestamp"))
+        timestampTextView.setTextColor(Color.GRAY)
+        messageLayout.addView(timestampTextView)
+
+        messageList.addView(messageLayout)
+    }
+
+    private fun timestampToString(epoch: Long): String {
+
+        val date = Date(epoch.toLong() * 1000)
+        val dateFormat = SimpleDateFormat("h:mm a", Locale.getDefault())
+        return dateFormat.format(date)
+    }
+
+    private fun currentTimestamp(): Long {
+        return System.currentTimeMillis() / 1000
     }
 }
--- Android Studio Project/app/src/main/java/com/example/androidstudioproject/MainActivity.kt
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
     private lateinit var accountsButton: Button
+    private lateinit var joinCreateButton: Button
     private lateinit var chatroomsButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +19,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         accountsButton = findViewById(R.id.accountsButton)
+        joinCreateButton = findViewById(R.id.joinCreateButton)
         chatroomsButton = findViewById(R.id.chatroomsButton)
 
         accountsButton.setOnClickListener{
             startActivity(Intent(this, Login::class.java))
         }
-        chatroomsButton.setOnClickListener{
+        joinCreateButton.setOnClickListener{
             startActivity(Intent(this, HomePage::class.java))
+        }
+        chatroomsButton.setOnClickListener{
+            startActivity(Intent(this, ChatRoom::class.java))
         }
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
