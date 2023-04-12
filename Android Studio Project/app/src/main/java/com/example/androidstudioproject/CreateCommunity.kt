842f876ac55cb0d3ba6e164ee0a5d9203bed6ec9
Youssef Yammine <lebenbou@gmail.com>
2023-04-12 17:03:22 +0300

    implemented create community backend

--- Android Studio Project/app/src/main/java/com/example/androidstudioproject/CreateCommunity.kt
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/CreateCommunity.kt
@@ -1,23 +1,140 @@
 package com.example.androidstudioproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlin.random.Random
 
 class CreateCommunity : AppCompatActivity() {
+
     private lateinit var communityNameInput: EditText
     private lateinit var descInput: EditText
+
     private lateinit var createComButton: Button
     private lateinit var goBackButton: Button
-    private val databaseClient=MongoClient()
+
+    private val databaseClient = MongoClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_community)
-        communityNameInput=findViewById(R.id.communityNameInput)
-        descInput=findViewById(R.id.descInput)
-        createComButton=findViewById(R.id.createComButton)
-        goBackButton=findViewById(R.id.goBackButton)
+
+        communityNameInput = findViewById(R.id.communityNameInput)
+        descInput = findViewById(R.id.descInput)
+
+        createComButton = findViewById(R.id.createComButton)
+        goBackButton = findViewById(R.id.goBackButton)
+
+        goBackButton.setOnClickListener{
+            onBackPressed()
+        }
+        createComButton.setOnClickListener{
+
+            val communityName = communityNameInput.text.toString().trim()
+            val desc = descInput.text.toString().trim()
+
+            if(communityName.isEmpty()) return@setOnClickListener showMessageBox("Empty Name","A community can't have an empty name.")
+            if(desc.isEmpty()) return@setOnClickListener showMessageBox("Empty Description","A community can't have an empty description.")
+
+            GlobalScope.launch {
+
+                runOnUiThread{ startLoadingMode() }
+
+                try {
+                    createCommunity(communityName, desc)
+                    startActivity(Intent(this@CreateCommunity, Community::class.java))
+
+                    runOnUiThread{ endLoadingMode() }
+                }
+                catch (e: Exception){
+                    runOnUiThread{ connectionDropped() }
+                }
+            }
+        }
+    }
+    private suspend fun createCommunity(name: String, desc: String){
+
+        var newCode = generateRandomCode() // 6 random letters/numbers
+        var response = JSONObject()
+
+        while(response.length() > 0){ // keep changing code until it is unique
+
+            newCode = generateRandomCode()
+            response = databaseClient.findOne("Communities", JSONObject().put("code", newCode))
+        }
+
+        GlobalVars.currentCommunityCode = newCode
+
+        val newRoom = JSONObject().apply {
+
+            put("code", newCode)
+            put("name", name)
+            put("description", desc)
+            put("creator", GlobalVars.currentUser)
+            put("users", JSONArray()) // empty list of messages
+            put("rooms", JSONArray()) // empty list of rooms
+        }
+        databaseClient.insertOne("Communities", newRoom)
+    }
+    private fun showMessageBox(title: String, message: String) {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+
+        builder.setPositiveButton("OK") { dialog, _ ->
+
+            endLoadingMode()
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+    private fun connectionDropped(){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Connection Failure")
+        builder.setMessage("Your internet connection dropped.\nPlease log back in.")
+            .setCancelable(false)
+            .setPositiveButton("OK") { _, _ ->
+                finish()
+                startActivity(Intent(this, Login::class.java))
+            }
+
+        val alert = builder.create()
+        alert.show()
+    }
+    private fun generateRandomCode(): String {
+
+        val charPool: List<Char> = ('A'..'Z') + ('0'..'9')
+        return (1..6)
+            .map { Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
+    }
+    private fun startLoadingMode(){
+
+        communityNameInput.isEnabled = false
+        goBackButton.isEnabled = false
+        createComButton.isEnabled = false
+
+        createComButton.setBackgroundResource(R.drawable.grey_btn_bg)
+        createComButton.text = "Creating..."
+    }
+    private fun endLoadingMode() {
+
+        communityNameInput.isEnabled = true
+        goBackButton.isEnabled = true
+        createComButton.isEnabled = true
+
+        createComButton.setBackgroundResource(R.drawable.green_btn_bg)
+        createComButton.text = "Create Community"
     }
 }
--- Android Studio Project/app/src/main/java/com/example/androidstudioproject/GlobalVars.kt
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/GlobalVars.kt
@@ -2,7 +2,9 @@ package com.example.androidstudioproject
 
 object GlobalVars{
 
+    var currentCommunityCode: String = "000000"
     var currentRoomCode: String = "000000"
     var currentUser: String = "not_logged_in"
+
     val MongoAPIKey: String = "9qpyQhdqGAHnWLPlK1Cl9zYEVTsjmuAJy8yNDyj54M9AS0VP8ZLVA8VWrMz4DvMR"
 }
