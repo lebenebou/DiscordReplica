610151453d39b2586410ff29ba1fc3828e12bff5
rajijo <joseph.raji@outlook.fr>
2023-04-19 23:34:20 +0300

    small changes and corrections

--- Audio Record Project/app/src/main/java/com/example/audiorecord_test3/MainActivity.kt
+++ Audio Record Project/app/src/main/java/com/example/audiorecord_test3/MainActivity.kt
@@ -10,8 +10,6 @@ import androidx.core.app.ActivityCompat
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import ws.schild.jave.*
-import java.io.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -68,31 +66,30 @@ class MainActivity : AppCompatActivity() {
             setUpRecording()
         }
     }
+
     fun buttonStop() {
         isActive= false
         audioTrack?.stop()
         audioRecord?.stop()
         audioRecord?.release()
         audioTrack?.release()
-    }
-
-    private fun playAudio(){
-
-        println("LIST OF theRecord: $theRecord")
-        println("SIZE OF theRecord: ${theRecord.size}")
 
         sendToServer(theRecord)
+        println("theRecord Not cleared $theRecord")
+        println("theRecord Not cleared, size: ${theRecord.size}")
+        //we clear the buffer after sending it
+        theRecord.clear()
+        println("theRecord cleared $theRecord")
+        println("theRecord cleared, size: ${theRecord.size}")
+    }
 
+
+    private fun playAudio(){
         playRecording()
-        theRecord.clear()// for testing purpose
     }
 
     @Override
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             0 -> {
@@ -137,16 +134,16 @@ class MainActivity : AppCompatActivity() {
         audioRecord!!.startRecording()
 
         isActive = true
-        audioThread = recording()
+        audioThread = startTheRecording()
 
         audioThread!!.start()
     }
-    private fun recording(): Thread {
+
+    private fun startTheRecording(): Thread {
         return Thread {//The thread prevents the code from blocking on the while loop and thus allows accessing the stop button
             while (isActive) {
                 //we read the bytes captured by audioRecord and save them in SHORT FORMAT inside shortAudioData (not bytes)
                 audioRecord!!.read(shortAudioData, 0, shortAudioData.size)
-
 
 
                 //we add all shortAudioData inside a buffer
@@ -161,8 +158,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun sendToServer(record: MutableList<Short>){
         val encoding = Encoding()
+        //element that we will send to the server
         compressedByteArray= encoding.compressList(record)
-        println("compressedByteArray.size ${compressedByteArray.length}")
+        println("this is the length of the String that we send to the server ${compressedByteArray.length}")
     }
 
     private fun receivedFromServer(compressedString:String):MutableList<Short>{
@@ -195,14 +193,17 @@ class MainActivity : AppCompatActivity() {
         audioTrack!!.play()
 
 
-        val theRecordAfterCompression = receivedFromServer(compressedByteArray)
+        val theVoiceToPlay = receivedFromServer(compressedByteArray)
+
+        println("TheVoiceToPlay which is theRecord that the user receive $theVoiceToPlay")
+        println("TheVoiceToPlay which is theRecord that the user receive, size: ${theVoiceToPlay.size}")
 
 
-        val shortAudioDataForPlaying = ShortArray(theRecordAfterCompression.size)
+        val shortAudioDataForPlaying = ShortArray(theVoiceToPlay.size)
 
         var i=0
-        while(i< theRecordAfterCompression.size){
-            shortAudioDataForPlaying[i]= theRecordAfterCompression[i]
+        while(i< theVoiceToPlay.size){
+            shortAudioDataForPlaying[i]= theVoiceToPlay[i]
             i++
         }
 
@@ -210,99 +211,5 @@ class MainActivity : AppCompatActivity() {
             audioTrack!!.write(shortAudioDataForPlaying, 0, shortAudioDataForPlaying.size)
         }
     }
-
-
-
-/**
-
-    private fun encodeShortListToMp3(audioData: MutableList<Short>): JSONObject {
-        val audio = ws.schild.jave.encode.AudioAttributes()
-        audio.setCodec("libmp3lame")
-        audio.setBitRate(128000)
-        audio.setChannels(2)
-        audio.setSamplingRate(44100)
-
-        val attrs = EncodingAttributes()
-        attrs.setOutputFormat("mp3")
-        attrs.setAudioAttributes(audio)
-
-        val encoder = Encoder()
-
-        val encodedData: ByteArray
-        val byteArrayInputStream = ByteArrayInputStream(AudioUtils.shortArrayToByteArray(audioData.toShortArray()))
-        val multimediaObject = MultimediaObject(byteArrayInputStream)
-
-        try {
-            val outputStream = ByteArrayOutputStream()
-            encoder.encode(multimediaObject, outputStream, attrs)
-            encodedData = outputStream.toByteArray()
-        } catch (ex: Exception) {
-            ex.printStackTrace()
-            return JSONObject()
-        }
-
-        val json = JSONObject()
-        json.put("data", Base64.encodeToString(encodedData, Base64.DEFAULT))
-        return json
-    }
-
-
-
-    object AudioUtils {
-        fun shortArrayToByteArray(data: ShortArray): ByteArray {
-            val bytes = ByteArray(data.size * 2)
-            for (i in data.indices) {
-                bytes[i * 2] = (data[i] and 0x00FF).toByte()
-                bytes[i * 2 + 1] = (data[i].toInt() shr 8).toByte()
-            }
-            return bytes
-        }
-    }
-
-
-    private fun convertTxtToMp3(source: File, target: File) {
-        try {
-            // Audio Attributes
-            val audio = ws.schild.jave.encode.AudioAttributes()
-            audio.setCodec("libmp3lame")
-            audio.setBitRate(128000)
-            audio.setChannels(2)
-            audio.setSamplingRate(44100)
-
-            // Encoding Attributes
-            val attrs = EncodingAttributes()
-            attrs.setOutputFormat("mp3")
-            attrs.setAudioAttributes(audio)
-
-            // Encode
-            val encoder = Encoder()
-            encoder.encode(MultimediaObject(source), target, attrs)
-        } catch (ex: Exception) {
-            ex.printStackTrace()
-        }
-    }
-
-    private fun convertMp3ToTxt(mp3File: File, txtFile: File) {
-        try {
-            // Audio Attributes
-            val audio = ws.schild.jave.encode.AudioAttributes()
-            audio.setCodec("pcm_s16le")
-            audio.setBitRate(16)
-            audio.setChannels(1)
-            audio.setSamplingRate(16000)
-
-            // Encoding Attributes
-            val attrs = EncodingAttributes()
-            attrs.setOutputFormat("s16le")
-            attrs.setAudioAttributes(audio)
-
-            // Encode
-            val encoder = Encoder()
-            encoder.encode(MultimediaObject(mp3File), txtFile, attrs)
-        } catch (ex: Exception) {
-            ex.printStackTrace()
-        }
-    }
-    **/
 
 }
