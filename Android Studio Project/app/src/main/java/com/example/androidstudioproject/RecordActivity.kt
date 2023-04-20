71a964f835df3001f81fbf5cde434f2c2e58a592
coutJP <118437370+coutJP@users.noreply.github.com>
2023-04-20 10:28:17 +0300

    Audiobt

--- Android Studio Project/app/src/main/java/com/example/androidstudioproject/RecordActivity.kt
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/RecordActivity.kt
@@ -2,10 +2,30 @@ package com.example.androidstudioproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RecordActivity : AppCompatActivity() {
+    private lateinit var playbutton: Button
+    private lateinit var startbutton: Button
+    private lateinit var stopbutton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
+
+        playbutton = findViewById(R.id.playbutton)
+        startbutton = findViewById(R.id.startbutton)
+        stopbutton = findViewById(R.id.stopbutton)
+        playbutton.setOnClickListener {
+            println("Play was pressed")
+        }
+        startbutton.setOnClickListener {
+            println("Start was pressed")
+        }
+        stopbutton.setOnClickListener {
+            println("stop was pressed")
+        }
+
     }
 }
