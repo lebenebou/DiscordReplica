ae0a5e2d3ffadec2794f9b25658800adec93521f
coutJP <118437370+coutJP@users.noreply.github.com>
2023-04-23 12:41:47 +0300

    search res on screen

--- Android Studio Project/app/src/main/java/com/example/androidstudioproject/SearchCommunity.kt
+++ Android Studio Project/app/src/main/java/com/example/androidstudioproject/SearchCommunity.kt
@@ -2,16 +2,26 @@ package com.example.androidstudioproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.SearchView
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import org.json.JSONArray
 
 class SearchCommunity : AppCompatActivity() {
     private lateinit var searchView:SearchView
-    private lateinit var searchButton:Button
+//    private lateinit var searchButton:Button
+    private lateinit var search_results_layout:LinearLayout
+    private lateinit var name_text_view:TextView
+    private lateinit var description_text_view:TextView
+    private lateinit var search_results_scroll_view:ScrollView
+
     private val databaseClient = MongoClient()
 
 
@@ -20,10 +30,14 @@ class SearchCommunity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_community)
         searchView = findViewById(R.id.searchView)
-        searchButton = findViewById(R.id.searchButton)
-        searchButton.setOnClickListener {
-            val query = searchView.query.toString()
-        }
+//        searchButton = findViewById(R.id.searchButton)
+        search_results_layout = findViewById(R.id.search_results_layout)
+        name_text_view = findViewById(R.id.name_text_view)
+        description_text_view = findViewById(R.id.description_text_view)
+        search_results_scroll_view=findViewById(R.id.search_results_scroll_view)
+//        searchButton.setOnClickListener {
+//            val query = searchView.query.toString()
+//        }
 
 
 
@@ -37,12 +51,12 @@ class SearchCommunity : AppCompatActivity() {
 
                 GlobalScope.launch {
                     val searchResults = databaseClient.getSearchResults("Communities", "name", query)
-                    println(searchResults);
+
+
                     runOnUiThread{
-                        //displayResults(searchReasults);
+                        createSearchResultViews(searchResults, search_results_layout)
                     }
                 }
-
 
                 return true
             }
@@ -55,6 +69,31 @@ class SearchCommunity : AppCompatActivity() {
         })
 
     }
+
+    fun processSearchResults(searchResults: JSONArray) {
+        for (i in 0 until searchResults.length()) {
+            val community = searchResults.getJSONObject(i)
+            val name = community.getString("name")
+            val description = community.getString("description")
+            // Do something with name and description
+            println("Name: $name, Description: $description")
+        }
+    }
+
+    fun createSearchResultViews(searchResults: JSONArray, searchResultsLayout: LinearLayout) {
+        for (i in 0 until searchResults.length()) {
+            val community = searchResults.getJSONObject(i)
+            val name = community.getString("name")
+            val description = community.getString("description")
+            val searchResultView = LayoutInflater.from(this@SearchCommunity).inflate(R.layout.activity_search_community, searchResultsLayout, false)
+            val nameTextView = searchResultView.findViewById<TextView>(R.id.name_text_view)
+            val descriptionTextView = searchResultView.findViewById<TextView>(R.id.description_text_view)
+            nameTextView.text = name
+            descriptionTextView.text = description
+            searchResultsLayout.addView(searchResultView)
+        }
+    }
+
 
     private fun showMessageBox(title: String, message: String) {
 
